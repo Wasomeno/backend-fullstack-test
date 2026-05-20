e789bf15680fc1272d5589382391b2db055b5a16
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-05-20 20:24:05 +0700

    feat: initial web api

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+// <snippet_UseSwagger>
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+// </snippet_UseSwagger>
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
