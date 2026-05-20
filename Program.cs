e80364170b907955f28265fad5727c4a6cb44bb2
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-05-20 21:06:48 +0700

    feat(api): add OpenAPI support with Swagger UI
    - Add Microsoft.AspNetCore.OpenApi package reference
    
    - Register OpenAPI service with AddOpenApi()
    
    - Map OpenAPI endpoint with MapOpenApi()

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 
@@ -12,6 +13,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwaggerUi(options =>
     {
         options.DocumentPath = "/openapi/v1.json";
