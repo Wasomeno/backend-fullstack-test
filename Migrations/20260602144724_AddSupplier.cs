812dfada3d532db13f83079ab2eea00ee54f237d
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 21:48:17 +0700

    feat: supplier migration

---
+++ Migrations/20260602144724_AddSupplier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_fullstack_test.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "supplier",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    address = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    phone_number = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_supplier", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "supplier");
+        }
+    }
+}
