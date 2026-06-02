017a06c2e9c599944b6f482703bbb87c8c2753de
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 20:54:47 +0700

    feat: product update dto

--- Domain/Product/Dtos/ProductUpdateDto.cs
+++ Domain/Product/Dtos/ProductUpdateDto.cs
@@ -6,6 +6,19 @@
         public string Unit { get; set; }
         public string SKU { get; set; }
         public string Weight { get; set; }
-    }
+        public Guid ProductCategoryId { get; set; }
 
+        public static WarehouseSystemTest.Models.Product ToModel(ProductCreateDto data)
+        {
+            var result = new WarehouseSystemTest.Models.Product
+            {
+                Name = data.Name,
+                Unit = data.Unit,
+                SKU = data.SKU,
+                Weight = data.Weight,
+                ProductCategoryId = data.ProductCategoryId
+            };
+            return result;
+        }
+    }
 }
