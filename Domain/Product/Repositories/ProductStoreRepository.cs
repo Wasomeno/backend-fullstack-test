45c0cd587c3f8a8ef1d28441bf89d6b02b3c585f
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 20:57:35 +0700

    feat: add update in the product store

--- Domain/Product/Repositories/ProductStoreRepository.cs
+++ Domain/Product/Repositories/ProductStoreRepository.cs
@@ -17,6 +17,15 @@ namespace WarehouseSystemTest.Domain.Product.Repositories
         {
             var created = _context.Add(payload);
             await _context.SaveChangesAsync();
+            await _context.Entry(created.Entity).Reference(p => p.ProductCategory).LoadAsync();
+            return created.Entity;
+        }
+
+        public async Task<WarehouseSystemTest.Models.Product> Update(WarehouseSystemTest.Models.Product payload)
+        {
+            var created = _context.Update(payload);
+            await _context.SaveChangesAsync();
+            await _context.Entry(created.Entity).Reference(p => p.ProductCategory).LoadAsync();
             return created.Entity;
         }
     }
