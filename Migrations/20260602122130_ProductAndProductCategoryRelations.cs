d07fd1225e8306736562c0b62cf72256003a7ebb
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 19:23:13 +0700

    feat: product and product category relations migration

---
+++ Migrations/20260602122130_ProductAndProductCategoryRelations.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_fullstack_test.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductAndProductCategoryRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategory",
+                principalColumn: "Id");
+        }
+    }
+}
