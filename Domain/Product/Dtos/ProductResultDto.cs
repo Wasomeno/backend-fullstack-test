25ad6c4a2ebac45051032f1e4f9f5c878cee7440
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 20:56:23 +0700

    feat: add poroduct category in the product result

--- Domain/Product/Dtos/ProductResultDto.cs
+++ Domain/Product/Dtos/ProductResultDto.cs
@@ -1,4 +1,4 @@
-﻿namespace WarehouseSystemTest.Domain.Product.Dto
+namespace WarehouseSystemTest.Domain.Product.Dto
 {
     public class ProductResultDto
     {
@@ -7,6 +7,7 @@
         public string Unit { get; set; }
         public string SKU { get; set; }
         public string Weight { get; set; }
+        public ProductCategoryDto? ProductCategory { get; set; }
 
         public ProductResultDto(Models.Product product)
         {
@@ -15,11 +16,19 @@
             Unit = product.Unit;
             SKU = product.SKU;
             Weight = product.Weight;
+            ProductCategory = product.ProductCategory != null
+                ? new ProductCategoryDto(product.ProductCategory)
+                : null;
         }
 
-        public static List<ProductResultDto> MapRepo(List<Models.Product> data)
-        {
-            return data?.Select(data => new ProductResultDto(data)).ToList() ?? [];
-        }
+        public static List<ProductResultDto> MapRepo(List<Models.Product> data) =>
+            data?.Select(d => new ProductResultDto(d)).ToList() ?? [];
+    }
+
+    public class ProductCategoryDto(Models.ProductCategory productCategory)
+    {
+        public Guid Id { get; set; } = productCategory.Id;
+        public string Name { get; set; } = productCategory.Name;
+        public string Code { get; set; } = productCategory.Code;
     }
 }
