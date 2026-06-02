6ecfe8e25880f6e5acdb73dbc502050a564a5072
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2026-06-02 20:56:52 +0700

    feat: add detail query in the product

--- Domain/Product/Repositories/ProductQueryRepository.cs
+++ Domain/Product/Repositories/ProductQueryRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using WarehouseSystemTest.Infrastructure.Database;
 using WarehouseSystemTest.Infrastructure.Repositories;
 
@@ -14,13 +15,14 @@ namespace WarehouseSystemTest.Domain.Product.Repositories
 
         public async Task<PaginationResult<WarehouseSystemTest.Models.Product>> Pagination()
         {
-            var query = _context.Product.AsQueryable();
+            var query = _context.Product.Include(p => p.ProductCategory).AsQueryable();
             var count = query.Count();
-
             var data = query.ToList();
 
             return new PaginationResult<WarehouseSystemTest.Models.Product> { Data = data, Count = count };
         }
-    }
 
+        public async Task<WarehouseSystemTest.Models.Product?> FindOneById(Guid id) =>
+            _context.Product.Include(p => p.ProductCategory).FirstOrDefault(p => p.Id == id);
+    }
 }
